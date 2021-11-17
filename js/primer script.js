e94a3c6953e6347686e9874a4789acdcf714f9ad
Alejandro Mileo <liuh.wow@gmail.com>
2021-11-17 16:43:52 -0300

    primer entrega V 1.0.0

---
+++ js/primer script.js
@@ -0,0 +1,7 @@
+let user_name = prompt("¡Hola! Nos encanta que nos visites. ¿Cómo te llamas?");
+const msg = "Gracias por visitarnos, " + user_name;
+const company_name = "Somos turismo, somos World Travelers :)"
+const reminder_msg =". No olvides que estamos de 8 am a 18 pm. " + company_name;
+
+
+alert(msg + reminder_msg);
