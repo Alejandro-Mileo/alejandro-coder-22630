275f8f8dfc9916b617166f30d0e357932f715b8d
Alejandro Mileo <liuh.wow@gmail.com>
2021-11-20 00:48:28 -0300

    Entrega v.1.1.0

--- js/primer script.js
+++ js/primer script.js
@@ -6,12 +6,12 @@
 //                                                                                                              ===
 // ================================================================================================================
 
-let user_name = prompt("¡Hola! Nos encanta que nos visites. ¿Cómo te llamas?");
-const msg = "Gracias por visitarnos, " + user_name;
-const company_name = "Somos turismo, somos World Travelers :)"
-const reminder_msg =". No olvides que estamos de 8 am a 18 pm. " + company_name;
+// let user_name = prompt("¡Hola! Nos encanta que nos visites. ¿Cómo te llamas?");
+// const msg = "Gracias por visitarnos, " + user_name;
+// const company_name = "Somos turismo, somos World Travelers :)"
+// const reminder_msg =". No olvides que estamos de 8 am a 18 pm. " + company_name;
 
-alert(msg + reminder_msg);
+// alert(msg + reminder_msg);
 
 
 // ================================================================================================================
@@ -31,42 +31,42 @@ alert(msg + reminder_msg);
 // ***********************************************************  
 
 
-let numeroUno = prompt("ingresa el primer numero");
-let numeroDos = prompt("ingresa el segundo numero");
+let numeroUno = parseInt(prompt("ingresa el primer numero"));
+let numeroDos = parseInt(prompt("ingresa el segundo numero"));
 
 if (numeroUno > numeroDos){
     alert("el primer numero es mayor");
 } else if(numeroDos > numeroUno){
     alert("el segundo numero es mayor");
 } 
-else (numeroUno == numeroDos);{
+else{
     alert("ambos numeros son iguales");
 }
 
 // ***********************************************************                                                                          
 //                                                         ===
 //                                                         === 
-//     RESULTADO NOTA PARCIAL + ALERT USANDO CONDICIONALES ===
+//   RESULTADO NOTA PARCIAL + ALERT USANDO CONDICIONALES   ===
 //                                                         ===
 //                                                         ===
 // ***********************************************************  
 
-let numberOne = parseInt(prompt("ingresa tu primer nota parcial"));
-let numberTwo  = parseInt(prompt("ingresa tu segunda nota parcial"));
-let numberThree = parseInt(prompt("ingresa tu tercer nota parcial"));
+// let numberOne = parseInt(prompt("ingresa tu primer nota parcial"));
+// let numberTwo  = parseInt(prompt("ingresa tu segunda nota parcial"));
+// let numberThree = parseInt(prompt("ingresa tu tercer nota parcial"));
 
-let result = parseInt(numberOne + numberTwo + numberThree) / 3;
-alert("Tu calificación final es: " + result.toFixed(2));
+// let result = parseInt(numberOne + numberTwo + numberThree) / 3;
+// alert("Tu calificación final es: " + result.toFixed(2));
 
-if (result <= 6){
-    alert("No has aprobado");
-}
-else if (result >= 7){
-    alert("Has aprobado");
-}
-else{
-    alert("El sistema solo acepta letras. Intenta de nuevo.");
-}
+// if (result <= 6){
+//     alert("No has aprobado");
+// }
+// else if (result >= 7){
+//     alert("Has aprobado");
+// }
+// else{
+//     alert("El sistema solo acepta letras. Intenta de nuevo.");
+// }
 
 
 // ***********************************************************                                                                          
@@ -78,31 +78,31 @@ else{
 // ***********************************************************  
 
 
-let precioMilla = 0;
-let destino = prompt("selecciona tu destino").toUpperCase();
-let millasRecorridas = parseInt(prompt("¿Cuántas millas recorriste?"));
-let litrosConsumidos = parseInt(prompt("¿Cuántos litros consumiste?"));
+// let precioMilla = 0;
+// let destino = prompt("selecciona tu destino").toUpperCase();
+// let millasRecorridas = parseInt(prompt("¿Cuántas millas recorriste?"));
+// let litrosConsumidos = parseInt(prompt("¿Cuántos litros consumiste?"));
 
 
-if (destino === "EUROPA" && destino != ""){
-    precioMilla = 20;
-}
-else if (destino === "ASIA" && destino != ""){
-    precioMilla = 30;
-}
-else if (destino === "AMERICA" && destino != ""){
-    precioMilla = 40;
-}
+// if (destino === "EUROPA" && destino != ""){
+//     precioMilla = 20;
+// }
+// else if (destino === "ASIA" && destino != ""){
+//     precioMilla = 30;
+// }
+// else if (destino === "AMERICA" && destino != ""){
+//     precioMilla = 40;
+// }
 
-let totalKms = millasRecorridas * precioMilla;
+// let totalKms = millasRecorridas * precioMilla;
 
 
-if (litrosConsumidos > 0 && litrosConsumidos <= 10){
-    totalKms = totalKms + 100;
-}
-else{
-    totalKms = totalKms + 200;
-}
+// if (litrosConsumidos > 0 && litrosConsumidos <= 10){
+//     totalKms = totalKms + 100;
+// }
+// else{
+//     totalKms = totalKms + 200;
+// }
 
-alert("tu precio final de tu vuelo es de: " + "$" + totalKms);
-console.log ("El precio final de tu vuelo es de: " + "$" + totalKms);
+// alert("El precio final de tu vuelo es de: " + "$" + totalKms);
+// console.log ("El precio final de tu vuelo es de: " + "$" + totalKms);
