2a321b788277d7ec19fa1d2a78671dbadcfa2178
ale coder <liuh.wow@gmail.com>
2021-12-28 16:33:29 -0300

    segunda preentrega

---
+++ js/carrito.js
@@ -0,0 +1,43 @@
+const carrito = JSON.parse(localStorage.getItem("carrito"));  // traigo la información del localstorage. Hago uso de la información del carrito.
+
+
+let tbody = document.querySelector(".tbody")     //señecciono la clase mediante el selector query
+
+function compraCarrito(infoCarrito){        // creo la función con el parámetro infoCarrito.
+
+    for(producto of infoCarrito){           // hago uso del for para iterar cada uno de los valores del array (precio, cantidad, id, etc, etc.)
+
+        let row = document.createElement("tr");         // creo una variable y le asigno un valor a crear en html.
+
+        row.innerHTML = `                              
+        <td>${producto.nombre}.</td>
+        <td>$${producto.precio}.-</td>
+        <td>${producto.cantidad}</td>
+        <td>${producto.id}</td>
+        <td><button class="btn btn-danger eliminarProducto" id="${producto.id}">Eliminar</button</td>
+
+        `                                               // conecto con la clase constructor de los items del carrito, así se añaden al html del carrito.
+
+        tbody.appendChild(row);                         // le indico que a tbody, se le añada su hijo (row) --> la variable (línea 10) puede llamarse de cualquier forma.
+    }
+
+}
+
+compraCarrito(carrito);
+
+let botonesEliminar = document.querySelectorAll(".eliminarProducto"); //selecciono todos los botones con id eliminar
+
+botonesEliminar.forEach(elemento => {
+    elemento.addEventListener("click", eliminarProducto)         // creo una función y le agrego un eventlistener el cual es "click" y lo conecto con la función
+})
+
+function eliminarProducto(e){
+
+    let index = carrito.findIndex (producto => producto.id == e.target.id);      // una vez conectada la función accedo al ID del item el cual puede ser eliminado de la lista.
+
+    carrito.splice(index, 1);  //elimina el item que el usuario elija. NO REMUEVE HTML, SOLO EL VALOR INTERNO.
+
+    e.target.parentNode.parentNode.remove();     // accedo al padre del padre del target. Es decir, al TR (html) del item seleccionado previamente
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));    // de esta forma modifico el localStorage para que se elimine el TR (html). Sin eso, el localstorage no recibe esa nueva indicación y aunque clickee 1231231 veces, el item NO va a eliminarse.
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-const carrito = [];
+let carrito = [];
 
-let divContainer = document.getElementById("row")
+let divContainer = document.getElementById("row")      //agarramos el input donde va a cargarse el código html
 
-function rellenarPagina(arrayProductos) {
+function rellenarPagina(arrayProductos) {             //una vez creada, le creamos una función y le pasamos un parámetro (arrayProductos)
 
-    for (let producto of arrayProductos) {
+    for (let producto of arrayProductos) {                      // acá recorremos/iteramos cada item/producto de la función (arrayproductos)
 
-        let div = document.createElement("div");
-        div.classList = "col-4 mt-3";
+        let div = document.createElement("div");                //creamos una nueva clase y le asignamos un valor. El valor es crear un elemento mediante un document.createElement("div"). Podría ser un Section, H1, etc.
+        div.classList = "col-2 mt-3";                           // le asigno una clase mediante un classList. "col-4 mt-3". 4 columnas y un margin top de 3. (Usamos bootstrap. DIOS BENDIGA A BOOTSTRAP)
 
-        div.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        div.innerHTML = `                                       
+        <div class="card" style="width: 100%;">
             <img src="${producto.imagen}" class="card-img-top" alt="${producto.id}">
                 <div class="card-body text-center">
                     <h5 class="card-title">${producto.nombre}</h5>
@@ -19,21 +19,30 @@ function rellenarPagina(arrayProductos) {
         
                 </div>
         </div>        
-        `
-        
+        `                                                                                                       //al div le agrego contenido mediante .innerHTML = y escribo cómo va a ser mi bloque de código en html. La gracia es crearla una vez desde JS 
+                                                                                                               // al iterarla, va a crear tantas Cards como productos 
         divContainer.appendChild(div)
     }
 
+    let carritoLocalStorage = JSON.parse(localStorage.getItem("carrito"));
+
+    if(carritoLocalStorage){
+        carritoNav(carritoLocalStorage);
+    }
+
+
+
 }
 
-rellenarPagina(productos);
+rellenarPagina(productos);                   // finalizamos llamando a la función y le pasamos un parámetro. ¿cuál? le pasamos productos porque es la "base" de cada card. Esto va a entrar a productos.js y va a leer el array de objeto uno por uno. De principio a fin.
 
 class Productocarrito{                                      //Hacemos esta clase previo al pusheo de la función anadirCarrito. Se va a pushear al array productos []  (es el array de objetos que contiene la info de cada card.)
-    constructor(nombre,precio,imagen,id){
-    this.nombre=nombre;
-    this.precio=precio;
-    this.imagen=imagen;
-    this.id=id;
+    constructor(nombre,precio,cantidad,id,subtotal){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = 1;
+    this.id = id;  
+    this.subtotal = precio;
     }
 }
 
@@ -45,22 +54,57 @@ botones.forEach(elemento =>{
 
 function anadirCarrito(e){                                  //esta función va a servir para recorrer cada nodo y cada hijo
 
-    // console.log(e.target.parentNode.children[0].textContent);                 //Busco el Nombre de la Card.
+    // console.log(e.target.parentNode.children[0].textContent);                 //Busco el NOMBRE de la Card.
     // console.log(e.target.parentNode.children[1].children[0].textContent);    //Busco el PRECIO de la Card.
     // console.log(e.target.parentNode.parentNode.children[0].src);            //Busco la IMAGEN de la Card.
     // console.log(e.target.parentNode.parentNode.children[0].alt)            //Busco el ID de la Card.
 
+    let carritoLocalStorage = JSON.parse(localStorage.getItem("carrito")); // Parseo la información que contiene carrito. Si no hago esto, cada vez que actualice la página, el navegador va a leer el array carrito [] pero vacío. (el array está vacío, pero le pusheo la información línea 64)
+
+    if(carritoLocalStorage){
+        carrito = carritoLocalStorage;        // de esta forma, aún si actualizo la página, la info va a quedar guardada dentro del localstorage, y en la variable carrito. (Carrito era una constante, pero me tiraba error)
+    } 
+
+    let index = carrito.findIndex(producto => producto.id == e.target.parentNode.parentNode.children[0].alt);  // Buscamos el index del array. Específicamente el ID. -> parentNode -> parentNoda -> children 0 (la posición que ocupa la información que necesito) y accedo al "alt"
 
     let nombre = e.target.parentNode.children[0].textContent;              // dentro de mi card, accedo al padre y busco al hijo en la posición 0, y el contenido textual. Es decir, el NOMBRE del producto.
     let precio = e.target.parentNode.children[1].children[0].textContent; // dentro de mi card, accedo al padre y busco al hijo en la posición 1, y el contenido textual. Es decir, el PRECIO del producto
     let imagen = e.target.parentNode.parentNode.children[0].src;         // acá es más rebuscado. Accedo al padre del padre y luego al hijo en la posición 0 y con src agarro la información de la IMAGEN
     let id = e.target.parentNode.parentNode.children[0].alt;             // lo mismo que la IMAGEN, solo que con esto consigo el ID del producto.
 
-    const producto = new Productocarrito (nombre, precio, imagen, id); // el nuevo objeto que creamos con la clase de la línea 31 a 38. Reemplazamos valores con cada Card.
     
-    carrito.push(producto);                                             //le pusheo el nuevo objeto "(producto)"" al array de carrito, línea 1.
+    if(index == -1){                                              
 
-    console.log(carrito);                                               //chequeo mediante un console.log el valor de mi nuevo objeto.
+        const producto = new Productocarrito(nombre, precio, imagen, id); // Creo un condicional. Si el index es -1 (Es decir, que no existe), lo va a crear con las propiedades del objeto (nombre, precio, imagen, id)
+        carrito.push(producto);    // luego pusheamos al carrito (línea 1) el nuevo producto creado (línea 84);
+    }
+    else{
+        carrito[index].cantidad++;                          // Pero si el valor del index SI exíste, entonces le agrega +1 ---> lo veo reflejado en carrito.html---> "cantidad"
+        carrito[index].subtotal = carrito[index].precio * carrito[index].cantidad;
+    }
+
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));       // uso localstorage para almacenar la información, aún si actualizo la página. Almacena CLAVE-VALOR
+
+    carritoNav(carrito);  // llamo a la función carritoNav para que se visualice la cantidad de items del carrito (en el nav!) *el parámetro es el array carrito de la línea 1*
 
 }
 
+function carritoNav (arrayCarrito){                      //  creamos una función con su parámetro
+
+    let textoCarrito = document.getElementById("carrito_id");        // igualamos una variable y tomamos el ID mediante el getelement
+
+    let totalProductos = 0;           // inicializa en 0 ya que el usuario se encarga de añadir items.
+
+    for (let producto of arrayCarrito){                         // uso el For para recorrer el  array
+        
+        totalProductos += producto.cantidad;                // totalProductos toma el valor inicial de 0 pero el valor va cambiando según el usuario elija nuevos productos. Igual y suma la cantidad de producto.cantidad
+
+    }
+
+    textoCarrito.innerHTML = "";                             // empieza vacío para no generar problemas si el usuario ya cargó items previamente
+    
+    textoCarrito.innerHTML = `<i class="fas fa-shopping-cart d-flex gap-3 text-warning"><p class="text-success textLine">(${totalProductos})</p></i> `        // el valor que va a tomar el output. entre paréntesis está el valor  de n cantidad de items.
+
+    // revisar líneas 27-28. Para que el carrito "no desaparezca" cuando actualizo la página
+}
